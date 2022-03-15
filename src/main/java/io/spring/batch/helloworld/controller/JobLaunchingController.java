a4c728292395a4a35f544aede098b4da96b4c96d
nara113 <0554my@naver.com>
2022-03-16 04:54:41 +0900

    REST 방식으로 잡 실행하기 - 잡 실행 전에 잡 파라미터 증가시키기

--- src/main/java/io/spring/batch/helloworld/controller/JobLaunchingController.java
+++ src/main/java/io/spring/batch/helloworld/controller/JobLaunchingController.java
@@ -3,6 +3,9 @@ package io.spring.batch.helloworld.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobLaunchingController {
     private final JobLauncher jobLauncher;
     private final ApplicationContext applicationContext;
+    private final JobExplorer jobExplorer;
 
     // curl -H "Content-Type: application/json" -X POST -d "{\"name\":\"explorerJob\",\"jobParameters\":{\"foo\":\"bar\"}}" 'http://localhost:8080/run'
     @PostMapping("/run")
     public ExitStatus runJob(@RequestBody JobLaunchRequest request) throws Exception {
         final Job job = applicationContext.getBean(request.getName(), Job.class);
 
-        return jobLauncher.run(job, request.getJobParameters()).getExitStatus();
+        // SimpleJobLauncher에서는 job 실행전에 파라미터를 조작해야한다.
+        JobParameters jobParameters =
+                new JobParametersBuilder(request.getJobParameters(), jobExplorer)
+                .getNextJobParameters(job)
+                .toJobParameters();
+
+        return jobLauncher.run(job, jobParameters).getExitStatus();
     }
 }
